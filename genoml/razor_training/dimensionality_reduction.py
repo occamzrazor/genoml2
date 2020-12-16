204dd3706ccc98a3d32cefefef6af33aff7780a0
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-16 09:21:30 -0800

    Update dimensionality_reduction.py

--- genoml/razor_training/dimensionality_reduction.py
+++ genoml/razor_training/dimensionality_reduction.py
@@ -12,54 +12,54 @@ class SelectFeatures:
     """
 
     def __init__(self,
-                 n_est:int = 50, 
-                 method:str = 'ExtraTrees',
-                 k:int = None
+                 n_est: int = 50,
+                 method: str = 'ExtraTrees',
+                 k: int = None
                  ):
 
         # Training set
         self.X = None
         self.y = None
-        
+
         self.n_est = n_est
         self.method = method
         self.k = k
         self.features_selected = None
-        
+
     @staticmethod
     def get_data(X, y):
         return X.index, X, y
-    
+
     @staticmethod
     def concat_data(X_reduced, index, y):
-        return pd.concat([index.reset_index(drop=True), 
-                          y.reset_index(drop=True), 
-                          X_reduced.reset_index(drop=True)], 
-                         axis = 1, 
+        return pd.concat([index.reset_index(drop=True),
+                          y.reset_index(drop=True),
+                          X_reduced.reset_index(drop=True)],
+                         axis=1,
                          ignore_index=False)
-             
+
     def get_reduced_dataset(self, X, y, model: Optional[ClassifierMixin] = None) -> pd.DataFrame:
         """
         Return training dataset containing features selected in the features_selected step.
         """
         index, self.X, self.y = self.get_data(X, y)
-        if not model: 
+        if not model:
             model = self.features_selection(self.method)
-            
+
         ID_df = pd.DataFrame(index)
         self.features_selected = model.get_support()
-        X_reduced = X.iloc[:,self.features_selected]
+        X_reduced = X.iloc[:, self.features_selected]
         return self.concat_data(X_reduced, ID_df, self.y)
-    
+
     def get_test_set_reduced(self, X_test, y_test):
         """
         Return test dataset containing features selected in the features_selected step.
         """
         index, X_test, y_test = self.get_data(X_test, y_test)
         ID_df_test = pd.DataFrame(index)
-        X_test_reduced = X_test.iloc[:,self.features_selected]
+        X_test_reduced = X_test.iloc[:, self.features_selected]
         return self.concat_data(X_test_reduced, ID_df_test, y_test)
-        
+
     def features_selection(self, method) -> ClassifierMixin:
         """
         Perform feature selection using an extra-trees classifier. 
@@ -73,5 +73,6 @@ class SelectFeatures:
                 raise ValueError("Have to pass number of features to be kept, k when using 'Univariate' method.")
             selected = SelectKBest(f_classif, k=self.k).fit(self.X, self.y)
         else:
-            raise ValueError("Only the methods 'ExtraTrees', and 'Univariate' are supported.") 
+            raise ValueError("Only the methods 'ExtraTrees', and 'Univariate' are supported.")
         return selected
+
