e05b750fdc13b7b2e1a404975c4c2fd5d98ecb1b
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-16 22:41:14 -0800

    Update rfecv_log_reg.py

--- genoml/razor_training/rfecv_log_reg.py
+++ genoml/razor_training/rfecv_log_reg.py
@@ -1,63 +1,48 @@
 import joblib
-from logistic_regression import RazorLogReg
 from sklearn.feature_selection import RFECV
 import pandas as pd
-from dimensionality_reduction import SelectFeatures
 import numpy as np
 from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
 
 def main(save_path):
-    """ Problem: Train and save logistic regression for various feature selection methods.
+    """ Problem: Train and save logistic regression for the RFECV method.
         
         Args:
-        model: Path to CSV file containing dataset for training.
-        X_train_path: Path to CSV file containing predictors's dataset for training.
-        y_train_path: Path to CSV file containing dependent variable's dataset for training.
         save_path: Path to save trained model.
     """
+    
     path = "/Users/mdmcastanos/genoml2/plink_files/"
     npy_file = path + "full_plink2_matrix.npy"
     tsv_file = path + "latest_labels.tsv"
     df_y = pd.read_csv(tsv_file, sep='\t')
-    df_X = pd.DataFrame(np.load(npy_file))
+    df = pd.DataFrame(np.load(npy_file))
 
-    df = df_X.assign(participant_id=df_y['participant_id'], case_control_other_latest=df_y['case_control_other_latest'])
+    df = df.assign(participant_id=df_y['participant_id'], case_control_other_latest=df_y['case_control_other_latest'])
     df = df.set_index('participant_id')
     df.reset_index(drop=True, inplace=True)
-
-    X = df.drop(columns=['case_control_other_latest'])
-    y = df['case_control_other_latest']
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3, random_state=42)
     
-    training_set = pd.concat([X_train, y_train], axis=1)
-    rfecv = RazorLogReg(train_set=training_set)
-    rfecv_log_reg = rfecv.logistic_regression()
-    selector = RFECV(rfecv_log_reg, step=1000, cv = 3)
-    trained_classifier = selector.fit_transform(X, y)(X_train, y_train)
+    train, test = model_selection.train_test_split(df, test_size=0.3, random_state=42)
+    
+    del df
+    del df_y
+    del test
+    
+    log_reg_fitted = LogisticRegression(penalty='elasticnet',
+                                        solver='saga',
+                                        multi_class='multinomial',
+                                        l1_ratio=0.5,
+                                        class_weight='balanced',
+                                        max_iter=1000,
+                                        n_jobs=-1
+                                      )
+                                      
+    selector = RFECV(log_reg_fitted, step=2000)
+    
+    y_train = train['case_control_other_latest']
+    train.drop(columns=['case_control_other_latest'], inplace=True)
+    trained_classifier = selector.fit(train, y_train)
     joblib.dump(trained_classifier, save_path)
-"""
-class train_models():
-    
-    def __init__(self,
-                 X_train,
-                 y_train):
-        # Import training data
-        self.X = X_train
-        self.y= y_train
 
-    def RFECV(self):
-        
-        # Concatenate data
-        training_set = pd.concat([X_train, y_train], axis=1)
-        
-        # Train Logistic Regression and apply RFECV
-        rfecv = RazorLogReg(train_set=training_set)
-        rfecv_log_reg = rfecv.logistic_regression()
-        selector = RFECV(rfecv_log_reg, step=1000, cv=3)
-        
-        # Return refitted model
-        return selector.fit(X_train, y_train)
-"""
 if __name__ == '__main__':
     main(save_path='RFECV_log_reg')
-
