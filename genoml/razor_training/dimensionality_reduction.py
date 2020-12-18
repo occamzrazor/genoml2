be2b22718da2e12c6e7ea60248fe60b067db84cd
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-18 15:53:35 -0800

    Update dimensionality_reduction.py

--- genoml/razor_training/dimensionality_reduction.py
+++ genoml/razor_training/dimensionality_reduction.py
@@ -1,78 +1,60 @@
 from sklearn import ensemble
 import pandas as pd
-import numpy as np
-from sklearn.feature_selection import SelectFromModel, SelectKBest, f_classif
-from typing import Tuple, Optional
+from sklearn.feature_selection import SelectFromModel, SelectKBest, f_classif, chi2, mutual_info_classif
+from typing import Optional
 from sklearn.base import ClassifierMixin
 
 
 class SelectFeatures:
     """
-    Implement Feature selection on high dimensional dataset.
+    Implement various feature selection methods.
     """
 
     def __init__(self,
-                 n_est: int = 50,
+                 k: int,
                  method: str = 'ExtraTrees',
-                 k: int = None
+                 test: str = None
                  ):
 
-        # Training set
-        self.X = None
-        self.y = None
-
-        self.n_est = n_est
         self.method = method
         self.k = k
+        self.test = test
         self.features_selected = None
-
-    @staticmethod
-    def get_data(X, y):
-        return X.index, X, y
-
-    @staticmethod
-    def concat_data(X_reduced, index, y):
-        return pd.concat([index.reset_index(drop=True),
-                          y.reset_index(drop=True),
-                          X_reduced.reset_index(drop=True)],
-                         axis=1,
-                         ignore_index=False)
 
     def get_reduced_dataset(self, X, y, model: Optional[ClassifierMixin] = None) -> pd.DataFrame:
         """
         Return training dataset containing features selected in the features_selected step.
         """
-        index, self.X, self.y = self.get_data(X, y)
         if not model:
-            model = self.features_selection(self.method)
+            model = self.features_selection(X, y, self.method)
 
-        ID_df = pd.DataFrame(index)
         self.features_selected = model.get_support()
-        X_reduced = X.iloc[:, self.features_selected]
-        return self.concat_data(X_reduced, ID_df, self.y)
+        X_reduced = X[:, self.features_selected]
+        return X_reduced
 
-    def get_test_set_reduced(self, X_test, y_test):
+    def get_test_set_reduced(self, X_test):
         """
         Return test dataset containing features selected in the features_selected step.
         """
-        index, X_test, y_test = self.get_data(X_test, y_test)
-        ID_df_test = pd.DataFrame(index)
-        X_test_reduced = X_test.iloc[:, self.features_selected]
-        return self.concat_data(X_test_reduced, ID_df_test, y_test)
+        X_test_reduced = X_test[:, self.features_selected]
+        return X_test_reduced
 
-    def features_selection(self, method) -> ClassifierMixin:
+    def features_selection(self, X, y, method) -> ClassifierMixin:
         """
         Perform feature selection using an extra-trees classifier. 
         """
-        selected = None
         if method == 'ExtraTrees':
-            selected_features = ensemble.ExtraTreesClassifier(n_estimators=self.n_est).fit(self.X, self.y)
+            selected_features = ensemble.ExtraTreesClassifier(n_estimators=self.k).fit(X, y)
             selected = SelectFromModel(selected_features, prefit=True)
         elif method == 'Univariate':
             if not self.k:
                 raise ValueError("Have to pass number of features to be kept, k when using 'Univariate' method.")
-            selected = SelectKBest(f_classif, k=self.k).fit(self.X, self.y)
+            if self.test == 'chi2':
+                selected = SelectKBest(chi2, k=self.k).fit(X, y)
+            elif self.test == 'mutual':
+                selected = SelectKBest(mutual_info_classif, k=self.k).fit(X, y)
+            else:
+                selected = SelectKBest(f_classif, k=self.k).fit(X, y)
         else:
             raise ValueError("Only the methods 'ExtraTrees', and 'Univariate' are supported.")
         return selected
-
