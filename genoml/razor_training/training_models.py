12dadfe88e0b0a483365b55082ce7bd1b82b62be
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-20 22:32:32 -0800

    Update training_models.py

--- genoml/razor_training/training_models.py
+++ genoml/razor_training/training_models.py
@@ -57,7 +57,7 @@ def select_features(filename, method=None, test=None):
 
 
 def main():
-    select_features('tree')
+    #select_features('tree')
     select_features('univariate_ftest', method='Univariate')
     select_features('univariate_chi2', method='Univariate', test='chi2')
     select_features('univariate_mutual', method='Univariate', test='mutual')
