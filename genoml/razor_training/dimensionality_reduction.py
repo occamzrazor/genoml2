27922a46719f608a193ad6769200c2c193b29ea5
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-20 19:26:54 -0800

    Update dimensionality_reduction.py

--- genoml/razor_training/dimensionality_reduction.py
+++ genoml/razor_training/dimensionality_reduction.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.feature_selection import SelectFromModel, SelectKBest, f_classif, chi2, mutual_info_classif
 from typing import Optional
 from sklearn.base import ClassifierMixin
+import numpy as np
 
 
 class SelectFeatures:
@@ -13,13 +14,15 @@ class SelectFeatures:
     def __init__(self,
                  k: int = 10000,
                  method: str = 'ExtraTrees',
-                 test: str = None
+                 test: str = None, 
+                 outfile: str
                  ):
 
         self.method = method
         self.k = k
         self.test = test
         self.features_selected = None
+        self.outfile = outfile
 
     def get_reduced_dataset(self, X, y, model: Optional[ClassifierMixin] = None) -> pd.DataFrame:
         """
@@ -27,10 +30,11 @@ class SelectFeatures:
         """
         if not model:
             model = self.features_selection(X, y)
-
-        #self.features_selected = model.get_support()
-        #X_reduced = X[:, self.features_selected]
-        return model
+        # Save selected features by the model
+        self.features_selected = model.get_support()
+        np.save(self.outfile, np.where(self.features_selected))
+        # Return reduced dataset
+        return model.transform(X)
 
     def get_test_set_reduced(self, X_test):
         """
@@ -39,7 +43,7 @@ class SelectFeatures:
         X_test_reduced = X_test[:, self.features_selected]
         return X_test_reduced
 
-    def features_selection(self, X, y) -> ClassifierMixin:
+    def features_selection(X, y, self) -> ClassifierMixin:
         """
         Perform feature selection using an extra-trees classifier. 
         """
@@ -48,11 +52,11 @@ class SelectFeatures:
             selected = SelectFromModel(selected_features, prefit=True)
         elif self.method == 'Univariate':
             if self.test == 'chi2':
-                selected = SelectKBest(chi2, k=self.k).fit_transform(X, y)
+                selected = SelectKBest(chi2, k=self.k).fit(X, y)
             elif self.test == 'mutual':
-                selected = SelectKBest(mutual_info_classif, k=self.k).fit_transform(X, y)
+                selected = SelectKBest(mutual_info_classif, k=self.k).fit(X, y)
             else:
-                selected = SelectKBest(f_classif, k=self.k).fit_transform(X, y)
+                selected = SelectKBest(f_classif, k=self.k).fit(X, y)
         else:
             raise ValueError("Only the methods 'ExtraTrees', and 'Univariate' are supported.")
         return selected
