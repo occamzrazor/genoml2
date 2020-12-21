5f9ee871fa0e8796630135a95e4dbd8d96071df7
mdmcastanos <58451024+mdmcastanos@users.noreply.github.com>
2020-12-20 22:03:57 -0800

    Update dimensionality_reduction.py

--- genoml/razor_training/dimensionality_reduction.py
+++ genoml/razor_training/dimensionality_reduction.py
@@ -1,9 +1,10 @@
 from sklearn import ensemble
 import pandas as pd
 from sklearn.feature_selection import SelectFromModel, SelectKBest, f_classif, chi2, mutual_info_classif
-from typing import Optional
+from typing import Optional, NoReturn
 from sklearn.base import ClassifierMixin
 import numpy as np
+from sklearn.decomposition import PCA
 
 
 class SelectFeatures:
@@ -15,7 +16,8 @@ class SelectFeatures:
                  k: int = 10000,
                  method: str = 'ExtraTrees',
                  test: str = None, 
-                 outfile: str = None
+                 outfile: str = None,
+                 run_PCA: bool = False
                  ):
 
         self.method = method
@@ -24,7 +26,7 @@ class SelectFeatures:
         self.features_selected = None
         self.outfile = outfile
 
-    def get_reduced_dataset(self, X, y, model: Optional[ClassifierMixin] = None) -> pd.DataFrame:
+    def get_reduced_dataset(self, X: np.array, y: np.array, model: Optional[ClassifierMixin] = None) -> np.array:
         """
         Return training dataset containing features selected in the features_selected step.
         """
@@ -35,16 +37,20 @@ class SelectFeatures:
         if outfile:
             np.save(self.outfile, np.where(self.features_selected))
         # Return reduced dataset
-        return model.transform(X)
+        X_reduced = model.transform(X)
+        if run_PCA: 
+            # Apply PCA on sellected features
+            X_reduced = self.PCA(X_reduced, y)
+        return X_reduced 
 
-    def get_test_set_reduced(self, X_test):
+    def get_test_set_reduced(self, X_test: np.array):
         """
         Return test dataset containing features selected in the features_selected step.
         """
         X_test_reduced = X_test[:, self.features_selected]
         return X_test_reduced
 
-    def features_selection(self, X, y) -> ClassifierMixin:
+    def features_selection(self, X: np.array, y: np.array) -> ClassifierMixin:
         """
         Perform feature selection using an extra-trees classifier. 
         """
@@ -61,3 +67,11 @@ class SelectFeatures:
         else:
             raise ValueError("Only the methods 'ExtraTrees', and 'Univariate' are supported.")
         return selected
+    
+    def PCA(self, X_reduced: np.array, y: np.array) -> np.array:
+        """
+        Perform PCA on reduced dataset
+        """
+        pca = PCA(n_components=0.95)
+        return pca.fit_transform(X_reduced, y) 
+        
